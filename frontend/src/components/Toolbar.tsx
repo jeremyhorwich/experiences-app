7857eaa9314d133d44a341de2719a8453c75e915
jeremyhorwich <jeremyahorwich@gmail.com>
2024-09-11 16:40:11 -0500

    create toolbar component

---
+++ frontend/src/components/Toolbar.tsx
@@ -0,0 +1,13 @@
+import "../styles/ToolbarStyles.css";
+
+function Toolbar() {
+  return (
+    <div className="toolbar">
+      <div className="centered-text">
+        Experience App (Name WIP)
+      </div>
+    </div>
+  )
+}
+
+export { Toolbar };
