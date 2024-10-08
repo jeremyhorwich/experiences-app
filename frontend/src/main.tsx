5e4c29b272a18a31b8b39c4e6038d81bd1c02de7
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-10-08 15:48:48 -0500

    create login-form (#7)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ExperienceInfo } from './pages/ExperienceInfo';
+import { LoginPage } from './pages/LoginPage';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ExperienceInfo />
+    <LoginPage />
   </StrictMode>,
 )
