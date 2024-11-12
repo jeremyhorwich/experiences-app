9a747fc45b902cd167caba7b3d34afc438e2a34c
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-11-12 12:26:05 -0600

    create get /users (multiple users at a time) (#12)

--- backend/presentation/user_routes.py
+++ backend/presentation/user_routes.py
@@ -1,11 +1,34 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from services.user_service import UserService
 
 router = APIRouter(prefix="/users")
 
+user_service_instance = UserService()
+
+
+def get_message_service() -> UserService:
+    return user_service_instance
+
+
+user_service_dependency = Annotated[UserService, Depends(get_message_service)]
+
+
+class UserIdsRequest(BaseModel):
+    user_ids: list[str]
+
 
 @router.get("/{user_id}")
-async def get_user(user_id: str):
-    service = UserService()
-    user = await service.get(user_id)
+async def get_user(user_id: str, user_service: user_service_dependency):
+    user = await user_service.get(user_id)
     return user
+
+
+@router.post("/")
+async def get_multiple_users(
+    request: UserIdsRequest, user_service: user_service_dependency
+):
+    users = await user_service.get_multiple(request.user_ids)
+    return users
--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -6,6 +6,10 @@ class UserService:
     def __init__(self) -> None:
         self.user_repository = UserRepository()
 
-    async def get(self, user_id) -> User | None:
+    async def get(self, user_id: str) -> User | None:
         user = await self.user_repository.get(id=user_id)
         return user
+
+    async def get_multiple(self, user_ids: list[str]) -> list[User] | None:
+        users = await self.user_repository.get_multiple(ids=user_ids)
+        return users
