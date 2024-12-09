7a9e279d00fa33feccd70ab583afd5f4e2899f36
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-12-09 16:57:57 -0600

    create PUT users/{user_id}/update_rating (#14)

--- backend/services/user_service.py
+++ backend/services/user_service.py
@@ -13,3 +13,12 @@ class UserService:
     async def get_multiple(self, user_ids: list[str]) -> list[User] | None:
         users = await self.user_repository.get_multiple(ids=user_ids)
         return users
+
+    async def update_rating(self, user_id: str, increment_value: int) -> str | None:
+        bounded_increment = increment_value
+        if increment_value < -1:
+            bounded_increment = -1
+        if increment_value > 1:
+            bounded_increment = 1
+        result = await self.user_repository.update_rating(user_id, bounded_increment)
+        return result
