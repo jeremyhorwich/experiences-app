00da859c05c891b9591e339370cf91be9bf4e494
jeremyhorwich <jeremyahorwich@gmail.com>
2024-12-21 12:13:09 -0600

    create POST authentication/login route

--- backend/presentation/authentication_routes.py
+++ backend/presentation/authentication_routes.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
+from dependencies import limiter
 from fastapi import APIRouter, Depends
 from presentation.models.PostCredentialRequest import PostCredentialRequest
 from services.authentication_service import AuthenticationService
+from starlette.requests import Request
 
 router = APIRouter(prefix="/authentication")
 
@@ -25,4 +27,15 @@ async def create_user(
     authentication_service: authentication_service_dependency,
 ):
     result = await authentication_service.create_new(request.credential)
+    return result
+
+
+@router.post("/login")
+@limiter.limit("10/minute")
+async def login(
+    request: Request,  # pylint: disable=unused-argument
+    credential_request: PostCredentialRequest,
+    authentication_service: authentication_service_dependency,
+):
+    result = await authentication_service.verify_login(credential_request.credential)
     return result
